b16aa13b766d1f0c3ebfc29a61d9e41950cac729
Remy <remy.bouche@hotmail.fr>
2016-04-08 16:31:01 +0200

    Ajout rectangle.h

--- sources/Image/rectangle.h
+++ sources/Image/rectangle.h
@@ -1,7 +1,32 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include <iostream>
+#include "shape.h"
+
+class Rectangle : public Shape{
+
+private:
+    int heigth;
+    int length;
+
+public:
+    // Constructors
+    Rectangle();
+    Rectangle(int _X, int _Y, int _heigth, int _length, int _r, int _g, int _b, char _alpha);
+    ~Rectangle();
+
+    // Setters
+    void setLength(int _length);
+    void setHeigth(int _heigth);
 
 
+    // Getters
+    int getHeigth();
+    int getLength();
+};
 
-#endif // RECTANGLE_H
+
+#endif
+
+
